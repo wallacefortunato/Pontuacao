5aaa8dde822babf5e20f8d43ede5d2c133b57feb
Wallace Fortunato <wallace.fortunato@engenharia.ufjf.br>
2022-12-26 23:24:24 -0300

    Update project

--- src/CalculadoraBonus.java
+++ src/CalculadoraBonus.java
@@ -1,3 +1,11 @@
 public class CalculadoraBonus {
     public int bonusDoDia;
+
+    public int bonus(Usuario usuario){
+        int multiplicador = bonusDoDia;
+
+        if (usuario.vip) multiplicador *= 5;
+
+        return multiplicador;
+    }
 }
--- src/RegistroPontos.java
+++ src/RegistroPontos.java
@@ -1,14 +1,19 @@
 public class RegistroPontos {
 
+    private CalculadoraBonus calculadoraBonus;
+    public RegistroPontos (CalculadoraBonus calculadoraBonus){
+        this.calculadoraBonus = calculadoraBonus;
+    }
+
     public void fazerUmComentario (Usuario usuario){
-        usuario.pontos += 3;
+        usuario.pontos += 3*calculadoraBonus.bonus(usuario);
     }
 
     public void criarUmTopico (Usuario usuario){
-        usuario.pontos += 5;
+        usuario.pontos += 5*calculadoraBonus.bonus(usuario);
     }
 
     public void darUmLike (Usuario usuario){
-        usuario.pontos += 1;
+        usuario.pontos += 1*calculadoraBonus.bonus(usuario);
     }
 }
